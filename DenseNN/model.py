7fa3b0a3ec056e58ac62fa5445cfbc67ad94f933
Movses11 <movses.ghazaryan@gmail.com>
2023-05-28 03:49:51 +0400

    added Binary cross entropy loss

--- DenseNN/model.py
+++ DenseNN/model.py
@@ -4,6 +4,9 @@ from layers import DenseLayer, Dropout, BatchNormalization
 
 class Model:
     def __init__(self, layer_arr, loss='mse'):
+        """
+        loss: mse for regression, bce for classification
+        """
         self.loss = loss
         prev_n = 0
         norm_cnt = 0
@@ -29,7 +32,7 @@ class Model:
     def fit(self, X, Y, epochs=50, batch_size=32, lr=0.001):
         if len(Y.shape) == 1:
             Y = Y.reshape(-1, 1)
-        scaling_factor = X.shape[0] // 32
+        scaling_factor = X.shape[0] // batch_size #num of batches
 
         for _ in range(epochs):
             print('epoch', _, end=' ')
@@ -44,6 +47,10 @@ class Model:
                 if self.loss == 'mse':
                     neuron_grads = 2 * (ans - Y[indx]) / Y.shape[1]
                     losses += np.mean((ans - Y[indx]) ** 2 / Y.shape[1])
+                elif self.loss == 'bce':
+                    epsilon = 1e-7
+                    neuron_grads = (-Y[indx] / (ans + epsilon) + (1 - Y[indx]) / (1 - ans + epsilon)) / 2
+                    losses -= np.mean(Y[indx] * np.log(ans + epsilon) + (1 - Y[indx]) * np.log(1 - ans + epsilon))
 
                 for i in range(len(self.layers) - 1, 0, -1):
                     neuron_grads = self.layers[i]._backpropagate(neuron_grads)
