1c3e8c45d2112d4b3ff32e49d4578e251ce63d18
Movses11 <movses.ghazaryan@gmail.com>
2023-06-04 03:42:47 +0400

    added train/test split

--- DenseNN/testing.py
+++ DenseNN/testing.py
@@ -3,11 +3,11 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 import numpy as np
 from model import Model
-from layers import DenseLayer, Dropout, BatchNormalization
+from layers import DenseLayer, Dropout
 from sklearn.metrics import roc_curve
 from matplotlib import pyplot as plt
 
-reg = True       # control regression or classification problem
+reg = False       # control regression or classification problem
 last_layer = 0
 loss = 0
 if reg:
@@ -27,17 +27,22 @@ model = Model([DenseLayer(neurons=x.shape[1], include_bias=False),
                DenseLayer(neurons=64, activation='relu'),
                Dropout(0.2),
                DenseLayer(neurons=64, activation='relu'),
-               BatchNormalization(),
                DenseLayer(neurons=64, activation='relu'),
                last_layer],  # classification
               loss=our_loss,
               )
-model.fit(X_train, y_train, epochs=60, batch_size=16, lr=0.001)
+model.fit(X_train, y_train, epochs=20, batch_size=32, lr=0.001)
 preds = model.predict(X_test)
+preds1 = model.predict(X_train)
 
 if reg:
-    print('mse =', np.mean(((preds - y_test) ** 2)), 'mae =', np.mean(np.abs(preds - y_test)))
+    print('train mse =', np.mean(((preds1 - y_train) ** 2)), 'mae =', np.mean(np.abs(preds1 - y_train)))
+    print('test mse =', np.mean(((preds - y_test) ** 2)), 'mae =', np.mean(np.abs(preds - y_test)))
+
 else:
     roc = roc_curve(y_test, preds)
-    plt.plot(roc[0], roc[1])
+    plt.plot(roc[0], roc[1], label='test')
+    roc = roc_curve(y_train, preds1)
+    plt.plot(roc[0], roc[1], label='train')
+    plt.legend()
     plt.show()
