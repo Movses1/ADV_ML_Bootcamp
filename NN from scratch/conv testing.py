e3f2deeeb799fc6fdd6e9feacaf5a236bc10d78c
Movses11 <movses.ghazaryan@gmail.com>
2023-06-24 13:03:43 +0400

    train/test split added

--- NN from scratch/conv testing.py
+++ NN from scratch/conv testing.py
@@ -10,6 +10,8 @@ data = load_digits(n_class=2)
 x = data['data'].reshape(data['data'].shape[0], 8, 8, 1)
 y = data['target']
 
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
 model = Model([InpLayer(shape=np.array([8, 8, 1])),
                Conv2D(kernel_size=(4, 3), stride=(1, 1), filters=10, activation='relu'),
                Conv2D(kernel_size=(3, 4), stride=(1, 1), filters=5, activation='relu'),
@@ -17,10 +19,13 @@ model = Model([InpLayer(shape=np.array([8, 8, 1])),
               loss='bce',
               )
 
-model.fit(x, y, epochs=30, batch_size=32, lr=1e-2)
+model.fit(X_train, y_train, epochs=30, batch_size=32, lr=1e-2)
 
-pred = model.predict(x)
-
-roc = roc_curve(y, pred)
+preds = model.predict(X_test)
+preds1 = model.predict(X_train)
+roc = roc_curve(y_test, preds)
 plt.plot(roc[0], roc[1], label='test')
+roc = roc_curve(y_train, preds1)
+plt.plot(roc[0], roc[1], label='train')
+plt.legend()
 plt.show()
