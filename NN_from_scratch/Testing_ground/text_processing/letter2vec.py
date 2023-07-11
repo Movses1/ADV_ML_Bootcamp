fixed multidense not resetting gradients

NN_from_scratch/Testing_ground/text_processing/letter2vec.py
@@ -14,15 +14,18 @@ class_cnt = train_data[0].shape[-1]
 out_cnt = 6
 
 model = Model([InpLayer(shape=np.array([class_cnt])),
-               DenseLayer(neurons=70, activation='relu', k_init='he_normal'),
-               DenseLayer(neurons=15, activation='linear', k_init='he_normal'),
-               DenseLayer(neurons=100, activation='relu', k_init='he_normal'),
+               #DenseLayer(neurons=70, activation='relu', k_init='he_normal'),
+               DenseLayer(neurons=15, activation='linear', k_init='glorot_uniform'),
+               #DenseLayer(neurons=100, activation='relu', k_init='he_normal'),
                MultiDense(out_cnt=out_cnt, neurons=class_cnt, activation='softmax', k_init='glorot_uniform')],
-              loss='cce',
+              loss='cce', optimizer='adagrad'
               )
 combined_data = train_data[0]
 for ind in range(1, len(train_data)):
     combined_data = np.append(combined_data, train_data[ind], axis=0)
+    if len(combined_data)>1e5:
+        break
+combined_data = combined_data[:int(1e5)]
 
 x = []
 y = []
@@ -35,4 +38,4 @@ y = np.array(y)
 
 print(x.shape, y.shape)
 
-model.fit(x, y, epochs=100, batch_size=32, lr=1e-3)
+model.fit(x, y, epochs=25, batch_size=32, lr=1e-3)